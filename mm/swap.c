dc5331df78659ffd24dfc257edd611aa6e4de592
samakshkambxj <samakshk4@gmail.com>
2026-08-31 05:15:19 +0530

    mm: Disable swap readahead clustering
    Disable swap readahead clustering by setting the default
    page-cluster value to 0.
    
    This avoids speculative clustered swap reads for Android workloads
    using ZRAM, reducing unnecessary swap I/O and memory activity.
    
    Signed-off-by: samakshkambxj <samakshk4@gmail.com>

--- mm/swap.c
+++ mm/swap.c
@@ -1106,13 +1106,6 @@ EXPORT_SYMBOL(pagevec_lookup_range_tag);
  */
 void __init swap_setup(void)
 {
-	unsigned long megs = totalram_pages() >> (20 - PAGE_SHIFT);
-
-	/* Use a smaller cluster for small-memory machines */
 	/* Disable swap readahead clustering for Android/ZRAM workloads. */
 	page_cluster = 0;
-	/*
-	 * Right now other parts of the system means that we
-	 * _really_ don't want to cluster much more
-	 */
 }
