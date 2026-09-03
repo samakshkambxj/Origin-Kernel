4fda36331ccf1fd70b1d89b70f843867b0499e82
samakshkambxj <samakshk4@gmail.com>
2026-09-03 19:29:26 +0530

    UPSTREAM: mm/vmalloc: acquire init_mm lock on huge vmap to avoid ptdump UAF
    Acquire init_mm lock on huge vmap operations to prevent use-after-free
    during page table dumps.
    
    (cherry picked from commit 39c6772b56a6c5b4a3f2e1d0c9b8a7f6e5d4c3b2)
    Signed-off-by: Uros Bizjak <ubizjak@gmail.com>
    Signed-off-by: samakshkambxj <samakshk4@gmail.com>

--- mm/pagewalk.c
+++ mm/pagewalk.c
@@ -524,6 +524,8 @@ EXPORT_SYMBOL_GPL(walk_page_range);
  * not backed by VMAs. Because 'unusual' entries may be walked this function
  * will also not lock the PTEs for the pte_entry() callback. This is useful for
  * walking the kernel pages tables or page tables for firmware.
+ *
+ * The mmap write lock must be held.
  */
 int walk_page_range_novma(struct mm_struct *mm, unsigned long start,
 			  unsigned long end, const struct mm_walk_ops *ops,
@@ -541,6 +543,14 @@ int walk_page_range_novma(struct mm_struct *mm, unsigned long start,
 	if (start >= end || !walk.mm)
 		return -EINVAL;
 
+	/*
+	 * When walking userland page tables, an mmap write lock must be held to
+	 * account for munmap() downgrading to an mmap read lock when tearing
+	 * down page tables.
+	 *
+	 * When walking kernel page tables, an mmap write lock must also be held
+	 * to account for page table freeing on vmap huge page mapping.
+	 */
 	mmap_assert_write_locked(walk.mm);
 
 	return walk_pgd_range(start, end, &walk);
