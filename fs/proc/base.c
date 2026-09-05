ab038e1fd08351bef21abf5bfbfa47150e8e107e
samakshkambxj <samakshk4@gmail.com>
2026-09-05 12:23:56 +0530

    fs: Integrate ZeroMount VFS path redirection driver
    Add the ZeroMount kernel driver for mountless module loading on rooted
    Android. The driver intercepts VFS path lookups via getname() hook to
    redirect file paths before the filesystem processes them, resulting in
    zero mount table entries.
    
    Key features:
    - /dev/zeromount miscdevice with ioctl interface (11 commands)
    - VFS path redirection via getname() hook
    - Directory entry injection for readdir/getdents
    - d_path spoofing for /proc/PID/fd symlinks
    - SELinux xattr injection for redirected files
    - statfs spoofing (EROFS magic for system partitions)
    - mmap metadata spoofing for /proc/PID/maps
    - 4096-bit bloom filter for fast path rejection
    - RCU-protected hash tables for lock-free concurrent reads
    - UID-based app exclusion support
    
    Based on the 60_zeromount-android14-6.1 patch from Super-Builders.
    Requires KernelSU/SUSFS patches already present in this kernel.
    
    Signed-off-by: samakshkambxj <samakshk4@gmail.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -107,6 +107,9 @@
 #include <trace/events/oom.h>
 #include <trace/hooks/sched.h>
 #include "internal.h"
+#ifdef CONFIG_ZEROMOUNT
+#include <linux/zeromount.h>
+#endif
 #include "fd.h"
 
 #include "../../lib/kstrtox.h"
@@ -1862,6 +1865,26 @@ static int do_proc_readlink(const struct path *path, char __user *buffer, int bu
 				len = -EFAULT;
 			kfree(tmp);
 			return len;
+		}
+	}
+#endif
+
+#ifdef CONFIG_ZEROMOUNT
+	if (!zeromount_should_skip() && path->dentry) {
+		struct inode *inode = d_backing_inode(path->dentry);
+		if (inode) {
+			char *vpath = zeromount_get_static_vpath(inode);
+			if (vpath) {
+				int vlen = strlen(vpath);
+				if (vlen > buflen)
+					vlen = buflen;
+				if (copy_to_user(buffer, vpath, vlen) == 0) {
+					kfree(vpath);
+					kfree(tmp);
+					return vlen;
+				}
+				kfree(vpath);
+			}
 		}
 	}
 #endif
